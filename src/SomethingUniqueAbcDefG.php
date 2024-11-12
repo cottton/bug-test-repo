c0a0048ff6527581e91ed1499fa5d96235cb22a9
Enrico Kälert <ek@meets-ecommerce.de>
2024-11-12 01:33:48 +0100

    test

---
+++ src/SomethingUniqueAbcDefG.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cottton\BugTestRepository;
+
+class SomethingUniqueAbcDefG
+{
+    const A = 1;
+    const B = 2;
+    const C = 3;
+    const D = 4;
+    const E = 5;
+}
+
+$val = SomethingUniqueAbcDefG::E;
